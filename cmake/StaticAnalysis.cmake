152a45fcb8d08a470ddab609f955f0856a9b9f82
Wangoda Francis <fwangoda@gmail.com>
2026-09-01 23:37:22 +0300

    Add static analysis configuration for Clang-Tidy and Cppcheck

--- cmake/StaticAnalysis.cmake
+++ cmake/StaticAnalysis.cmake
@@ -1,6 +1,15 @@
 find_program(CLANG_TIDY_EXE NAMES clang-tidy)
 find_program(CPPCHECK_EXE NAMES cppcheck)
 
+if(UPGRADE_GUARD_ENABLE_STATIC_ANALYSIS)
+  if(CLANG_TIDY_EXE)
+    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXE};--config-file=${CMAKE_SOURCE_DIR}/.clang-tidy")
+  endif()
+  if(CPPCHECK_EXE)
+    set(CMAKE_CXX_CPPCHECK "${CPPCHECK_EXE};--enable=warning,performance,portability;--inline-suppr")
+  endif()
+endif()
+
 function(upgrade_guard_enable_clang_tidy target)
   if(CLANG_TIDY_EXE)
     set_property(TARGET ${target} PROPERTY CXX_CLANG_TIDY ${CLANG_TIDY_EXE})
